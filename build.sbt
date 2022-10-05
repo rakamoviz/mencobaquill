8d62bc6d3f428b87c736cfb5cc743384d49554dd
Cokorda Raka Angga Jananuraga <raka.moviz@gmail.com>
2022-10-05 11:13:56 -0500

    initial

---
+++ build.sbt
@@ -0,0 +1,19 @@
+import Dependencies._
+
+ThisBuild / scalaVersion     := "2.13.8"
+ThisBuild / version          := "0.1.0-SNAPSHOT"
+ThisBuild / organization     := "com.example"
+ThisBuild / organizationName := "example"
+
+libraryDependencies ++= Seq(
+  "io.getquill"          %% "quill-jdbc" % "3.4.10",
+  "org.postgresql"       %  "postgresql"     % "42.2.8"
+)
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "mencobaquill",
+    libraryDependencies += scalaTest % Test
+  )
+
+// See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
---
+++ src/main/scala/example/Hello.scala
@@ -0,0 +1,67 @@
+package example
+
+import io.getquill._
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
+
+case class PII(value: String)
+
+case class City(id: Int, 
+                name: PII, 
+                countryCode: String, 
+                district: String, 
+                population: Int)
+
+case class Country(code: String, 
+                   name: PII, 
+                   continent: String, 
+                   region: String,
+                   surfaceArea: Double,
+                   indepYear: Option[Int],
+                   population: Int,
+                   lifeExpectancy: Option[Double],
+                   gnp: Option[scala.math.BigDecimal],
+                   gnpold: Option[scala.math.BigDecimal],
+                   localName: String,
+                   governmentForm: String,
+                   headOfState: Option[String],
+                   capital: Option[Int],
+                   code2: String)
+
+case class CountryLanguage(countrycode: String, 
+                           language: String,
+                           isOfficial: Boolean,
+                           percentage: Double)
+
+
+object Hello extends Greeting with App {
+  println(greeting)
+  implicit val encodePII = MappedEncoding[PII, String]{value =>
+    println("I AM ENCODING")
+    val ret: String = s"${value.value}_xxx"
+    ret
+  }
+  
+  implicit val decodePII = MappedEncoding[String, PII]{ value =>
+    println("I AM DECODING")
+    val ret : PII = PII(value.dropRight(4))
+    ret
+  }
+
+  val pgDataSource = new org.postgresql.ds.PGSimpleDataSource()
+  pgDataSource.setUser("raka")
+  pgDataSource.setPassword("password")
+  pgDataSource.setDatabaseName("raka")
+  val config = new HikariConfig()
+  config.setDataSource(pgDataSource)
+  val ctx = new PostgresJdbcContext(LowerCase, new HikariDataSource(config))
+  import ctx._
+
+  val res = ctx.run(query[City]).filter(_.id == 3208)
+  println(res)
+
+  ctx.run(query[City].insert(City(20003, PII("cest"), "CST", "Cest City", 0)))
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello"
+}
