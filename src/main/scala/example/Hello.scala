8050f942714f03e095cafc2bbf13ea37fe9a83dd
Cokorda Raka Angga Jananuraga <raka.moviz@gmail.com>
2022-10-05 12:20:25 -0500

    works

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -3,37 +3,34 @@ package example
 import io.getquill._
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 
-case class PII(value: String)
-
-case class City(id: Int, 
-                name: PII, 
-                countryCode: String, 
-                district: String, 
-                population: Int)
-
-case class Country(code: String, 
-                   name: PII, 
-                   continent: String, 
-                   region: String,
-                   surfaceArea: Double,
-                   indepYear: Option[Int],
-                   population: Int,
-                   lifeExpectancy: Option[Double],
-                   gnp: Option[scala.math.BigDecimal],
-                   gnpold: Option[scala.math.BigDecimal],
-                   localName: String,
-                   governmentForm: String,
-                   headOfState: Option[String],
-                   capital: Option[Int],
-                   code2: String)
-
-case class CountryLanguage(countrycode: String, 
+object Hello extends Greeting with App {
+  case class PII(value: String)
+  case class City(id: Int, 
+                  name: PII, 
+                  countryCode: String, 
+                  district: String, 
+                  population: Int)
+  
+  case class Country(code: String, 
+                     name: PII, 
+                     continent: String, 
+                     region: String,
+                     surfaceArea: Double,
+                     indepYear: Option[Int],
+                     population: Int,
+                     lifeExpectancy: Option[Double],
+                     gnp: Option[scala.math.BigDecimal],
+                     gnpold: Option[scala.math.BigDecimal],
+                     localName: String,
+                     governmentForm: String,
+                     headOfState: Option[String],
+                     capital: Option[Int],
+                     code2: String)
+  
+  case class CountryLanguage(countrycode: String, 
                            language: String,
                            isOfficial: Boolean,
                            percentage: Double)
-
-
-object Hello extends Greeting with App {
   println(greeting)
 
   val pgDataSource = new org.postgresql.ds.PGSimpleDataSource()
@@ -45,9 +42,9 @@ object Hello extends Greeting with App {
   val ctx = new PostgresJdbcContext(LowerCase, new HikariDataSource(config))
   import ctx._
 
-  implicit val encodePII = MappedEncoding[PII, String]{value =>
+  implicit val encodePII = MappedEncoding[PII, String]{ pii =>
     println("I AM ENCODING")
-    val ret: String = s"${value.value}_xxx"
+    val ret: String = s"${pii.value}_xxx"
     ret
   }
   
@@ -60,7 +57,7 @@ object Hello extends Greeting with App {
   val res = ctx.run(query[City]).filter(_.id == 3208)
   println(res)
 
-  ctx.run(query[City].insert(City(20004, PII("dest"), "DST", "Dest City", 0)))
+  ctx.run(query[City].insert(City(20009, lift(PII("iest")), "IST", "Iest City", 0)))
 }
 
 trait Greeting {
